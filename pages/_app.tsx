f405866460b9d1c9bda5b7df0844d7de94d0e3da
Oleg Kamenshikov <burnsred94@gmail.com>
2022-08-14 22:37:08 +0300

    feat: htag implements and document add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AppProps } from 'next/app';
+import  Head  from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <>
+      <Head>
+        <title>Belkamen - Благоустройства, захоронения, установка памятников и оград</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet" />
+      </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp;
