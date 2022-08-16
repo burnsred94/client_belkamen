e6a806dae29b1f1542d0194ab9b6ec960e813166
Oleg Kamenshikov <burnsred94@gmail.com>
2022-08-16 17:14:10 +0300

    fix: component paragraph

--- components/Paragraph/Paragraph.tsx
+++ components/Paragraph/Paragraph.tsx
@@ -1,5 +1,18 @@
+import { ParagraphProps } from "./Paragraph.props";
+import cn from 'classnames';
+import styles from 'Paragraph.module.css';
 
-
-export const Paragraph = (): JSX.Element => {
-    return (<></>);
+export const Paragraph = ({ type, color, children, className, ...props}: ParagraphProps): JSX.Element => {
+    return (
+        <p className={cn(styles.paragraph, className, {
+            [styles.xsmall]: type == "xsmall",
+            [styles.small]: type == "small",
+            [styles.medium]: type == "medium",
+            [styles.large]: type == "large",
+            [styles.black]: color == "black",
+            [styles.primary]: color == "primary",
+        })} {...props}>
+            {children}
+        </p>
+    );
 }
